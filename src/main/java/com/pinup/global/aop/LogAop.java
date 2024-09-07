bf0f6aa4546514c19047b296cf7b375c652faa2b
Bellwin98 <jongseunghan@naver.com>
2024-09-07 23:14:02 +0900

    [Init] controller 및 service 단 시작과 끝에 로그 적용

---
+++ src/main/java/com/pinup/global/aop/LogAop.java
@@ -0,0 +1,24 @@
+package com.pinup.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAop {
+
+    // controller와 service method만 로그 적용
+    @Around("execution(* com.pinup.domain..controller..*(..)) || execution(* com.pinup.domain..service..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("START: {}", joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            log.info("END: {}", joinPoint.toString());
+        }
+    }
+}
