d43f8960cffe98d41222699c49a158d95389a07b
Bellwin98 <jongseunghan@naver.com>
2024-10-14 00:55:53 +0900

    [Feat] 리뷰 등록 API 리팩토링(#7)
    * 각 기능별 메서드로 분리
    * 추후 Mocking 라이브러리를 활용하여 단위 테스트 코드 작성 예정

--- src/main/java/com/pinup/service/ReviewService.java
+++ src/main/java/com/pinup/service/ReviewService.java
@@ -18,8 +18,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -41,76 +41,106 @@ public class ReviewService {
                                    PlaceRequest placeRequest,
                                    List<MultipartFile> images) {
 
-        List<String> uploadedFileUrls = new ArrayList<>();
-        List<String> inputKeywords = new ArrayList<>();
-
-        // 유저 존재 여부 확인
         Member findMember = findMember();
-
-        // 업체 카카오맵 ID
-        String kakaoPlaceId = placeRequest.getKakaoPlaceId();
-
-        /**
-         * DB에 업체 존재 여부 확인
-         * 업체 존재 시: 카카오맵 ID로 등록된 업체 조회
-         * 업체 미존재 시: DB에 해당 업체 등록
-         */
-        Optional<Place> findPlaceByKakaoMapId = placeRepository.findByKakaoMapId(kakaoPlaceId);
-        Place place;
-        if (findPlaceByKakaoMapId.isEmpty()) {
-            place = placeRequest.toEntity();
-        } else {
-            place = placeRepository.findByKakaoMapId(kakaoPlaceId)
-                    .orElseThrow(() -> PinUpException.PLACE_NOT_FOUND);
-        }
-
-        Place savedPlace = placeRepository.save(place);
-
-        Review newReview = reviewRequest.toEntity();
-
-        newReview.attachMember(findMember);
-        newReview.attachPlace(savedPlace);
-
-        // 등록된 리뷰 이미지가 있으면 S3에 저장 후 URL을 ReviewImage 엔티티에 저장
-        if (images != null && !images.get(0).getOriginalFilename().isEmpty()) {
-            if (images.size() > IMAGES_LIMIT) {
-                throw PinUpException.IMAGES_LIMIT_EXCEEDED;
-            }
-
-            for (MultipartFile multipartFile : images) {
-                String uploadedFileUrl = s3Service.uploadFile(FILE_TYPE, multipartFile);
-                ReviewImage reviewImage = new ReviewImage(uploadedFileUrl);
-                reviewImage.attachReview(newReview);
-                uploadedFileUrls.add(uploadedFileUrl);
-            }
-        }
-
-        // 등록된 키워드가 있으면 Keyword 엔티티에 저장
-        if (reviewRequest.getKeywords() != null && !reviewRequest.getKeywords().isEmpty()) {
-
-            if (reviewRequest.getKeywords().size() > KEYWORDS_LIMIT) {
-                throw PinUpException.KEYWORDS_LIMIT_EXCEEDED;
-            }
-
-            for (String comment : reviewRequest.getKeywords()) {
-                if (comment.length() > KEYWORDS_LENGTH_LIMIT) {
-                    throw PinUpException.KEYWORDS_LENGTH_LIMIT_EXCEEDED;
-                }
-                Keyword keyword = new Keyword(comment);
-                keyword.attachReview(newReview);
-                inputKeywords.add(comment);
-            }
-        }
-
+        Place place = findOrCreatePlace(placeRequest);
+        List<String> uploadedFileUrls = uploadImages(images);
+        List<String> inputKeywords = saveKeywords(reviewRequest);
+        Review newReview = createReview(reviewRequest, findMember, place, uploadedFileUrls, inputKeywords);
         Review savedReview = reviewRepository.save(newReview);
 
         return ReviewResponse.of(savedReview, uploadedFileUrls, inputKeywords);
     }
 
+    /**
+     * SecurityContextHolder 에서 현재 로그인 한 사용자 이메일 Get 후 회원 정보 탐색
+     */
     private Member findMember() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         return memberRepository.findByEmail(authentication.getName())
                 .orElseThrow(() -> PinUpException.MEMBER_NOT_FOUND);
+    }
+
+    /**
+     * 카카오맵 ID로 DB에 등록된 업체 정보 조회
+     * DB에 업체 미등록 시, 업체 신규 생성
+     */
+    private Place findOrCreatePlace(PlaceRequest placeRequest) {
+        String kakaoPlaceId = placeRequest.getKakaoPlaceId();
+
+        return placeRepository.findByKakaoMapId(kakaoPlaceId)
+                .orElseGet(() -> placeRepository.save(placeRequest.toEntity()));
+    }
+
+    /**
+     * S3에 이미지 업로드
+     * 이미지 최대 업로드 가능 갯수: 3장
+     */
+    private List<String> uploadImages(List<MultipartFile> images) {
+
+        if (images == null || images.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        if (images.size() > IMAGES_LIMIT) {
+            throw PinUpException.IMAGES_LIMIT_EXCEEDED;
+        }
+
+        List<String> uploadedFileUrls = new ArrayList<>();
+
+        for (MultipartFile multipartFile : images) {
+            String uploadedFileUrl = s3Service.uploadFile(FILE_TYPE, multipartFile);
+            uploadedFileUrls.add(uploadedFileUrl);
+        }
+
+        return uploadedFileUrls;
+    }
+
+    /**
+     * 키워드 저장
+     * 키워드 최대 10개 등록 가능
+     * 각 키워드 글자 수는 최대 10자
+     */
+    private List<String> saveKeywords(ReviewRequest reviewRequest) {
+        List<String> inputKeywords = new ArrayList<>();
+
+        if (reviewRequest.getKeywords() != null && !reviewRequest.getKeywords().isEmpty()) {
+            if (reviewRequest.getKeywords().size() > KEYWORDS_LIMIT) {
+                throw PinUpException.KEYWORDS_LIMIT_EXCEEDED;
+            }
+
+            for (String inputKeyword : reviewRequest.getKeywords()) {
+                if (inputKeyword.length() > KEYWORDS_LENGTH_LIMIT) {
+                    throw PinUpException.KEYWORDS_LENGTH_LIMIT_EXCEEDED;
+                }
+                inputKeywords.add(inputKeyword);
+            }
+        }
+
+        return inputKeywords;
+    }
+
+    /**
+     * 생성된 리뷰 엔티티에 작성자, 업체, 리뷰 이미지, 키워드 연결
+     */
+    private Review createReview(ReviewRequest reviewRequest, Member writer, Place place,
+                                List<String> uploadedFileUrls, List<String> inputKeywords) {
+
+        Review newReview = reviewRequest.toEntity();
+        newReview.attachMember(writer);
+        newReview.attachPlace(place);
+
+        // 리뷰 이미지와 키워드를 리뷰 엔티티에 연결
+        for (String fileUrl : uploadedFileUrls) {
+            ReviewImage reviewImage = new ReviewImage(fileUrl);
+            reviewImage.attachReview(newReview);
+        }
+
+        for (String inputKeyword : inputKeywords) {
+            Keyword keyword = new Keyword(inputKeyword);
+            keyword.attachReview(newReview);
+        }
+
+        return newReview;
     }
 }
