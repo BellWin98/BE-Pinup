aa93faa3d1899260360cb258995c5db0f4fa5428
Bellwin98 <jongseunghan@naver.com>
2024-11-03 00:40:13 +0900

    [Chore] Kafka 주석 해제 및 Kafka에 개발계 서버 IP 추가
    * 장소 API 인증 권한 필요하도록 수정

--- src/main/java/com/pinup/service/FriendRequestService.java
+++ src/main/java/com/pinup/service/FriendRequestService.java
@@ -3,7 +3,7 @@ package com.pinup.service;
 import com.pinup.dto.response.FriendRequestResponse;
 import com.pinup.entity.FriendRequest;
 import com.pinup.entity.Member;
-//import com.pinup.global.kafka.KafkaProduceService;
+import com.pinup.global.kafka.KafkaProduceService;
 import com.pinup.repository.FriendRequestRepository;
 import com.pinup.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,31 +24,31 @@ public class FriendRequestService {
     private final FriendRequestRepository friendRequestRepository;
     private final MemberRepository memberRepository;
     private final FriendShipService friendShipService;
-//    private final KafkaProduceService kafkaProduceService;
+    private final KafkaProduceService kafkaProduceService;
 
-//    @Transactional
-//    public FriendRequestResponse sendFriendRequest(Long receiverId) {
-//        String senderEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-//
-//        Member sender = memberRepository.findByEmail(senderEmail)
-//                .orElseThrow(() -> MEMBER_NOT_FOUND);
-//        Member receiver = memberRepository.findById(receiverId)
-//                .orElseThrow(() -> MEMBER_NOT_FOUND);
-//
-//        validateSelfFriendRequest(sender, receiver);
-//        validateDuplicateFriendRequest(sender, receiver);
-//
-//        FriendRequest friendRequest = FriendRequest.builder()
-//                .sender(sender)
-//                .receiver(receiver)
-//                .build();
-//        friendRequestRepository.save(friendRequest);
-//
-//        kafkaProduceService.sendNotification(receiver.getEmail(),
-//                sender.getName() + "님이 친구 요청을 보냈습니다.");
-//
-//        return FriendRequestResponse.from(friendRequest);
-//    }
+    @Transactional
+    public FriendRequestResponse sendFriendRequest(Long receiverId) {
+        String senderEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Member sender = memberRepository.findByEmail(senderEmail)
+                .orElseThrow(() -> MEMBER_NOT_FOUND);
+        Member receiver = memberRepository.findById(receiverId)
+                .orElseThrow(() -> MEMBER_NOT_FOUND);
+
+        validateSelfFriendRequest(sender, receiver);
+        validateDuplicateFriendRequest(sender, receiver);
+
+        FriendRequest friendRequest = FriendRequest.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .build();
+        friendRequestRepository.save(friendRequest);
+
+        kafkaProduceService.sendNotification(receiver.getEmail(),
+                sender.getName() + "님이 친구 요청을 보냈습니다.");
+
+        return FriendRequestResponse.from(friendRequest);
+    }
 
     private void validateSelfFriendRequest(Member sender, Member receiver) {
         if (sender.getEmail().equals(receiver.getEmail())) {
@@ -63,38 +63,38 @@ public class FriendRequestService {
                 });
     }
 
-//    @Transactional
-//    public FriendRequestResponse acceptFriendRequest(Long friendRequestId) {
-//        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId)
-//                .orElseThrow(() -> FRIEND_REQUEST_NOT_FOUND);
-//        validateRequestReceiverIsCurrentUser(friendRequest);
-//        validateFriendRequestStatus(friendRequest);
-//
-//        friendRequest.accept();
-//        friendRequestRepository.save(friendRequest);
-//        friendShipService.createFriendShip(friendRequest.getSender(), friendRequest.getReceiver());
-//
-//        kafkaProduceService.sendNotification(friendRequest.getSender().getEmail(),
-//                friendRequest.getReceiver().getName() + "님이 친구 요청을 수락했습니다.");
-//
-//        return FriendRequestResponse.from(friendRequest);
-//    }
+    @Transactional
+    public FriendRequestResponse acceptFriendRequest(Long friendRequestId) {
+        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId)
+                .orElseThrow(() -> FRIEND_REQUEST_NOT_FOUND);
+        validateRequestReceiverIsCurrentUser(friendRequest);
+        validateFriendRequestStatus(friendRequest);
 
-//    @Transactional
-//    public FriendRequestResponse rejectFriendRequest(Long friendRequestId) {
-//        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId)
-//                .orElseThrow(() -> FRIEND_REQUEST_NOT_FOUND);
-//        validateRequestReceiverIsCurrentUser(friendRequest);
-//        validateFriendRequestStatus(friendRequest);
-//
-//        friendRequest.reject();
-//        friendRequestRepository.save(friendRequest);
-//
-//        kafkaProduceService.sendNotification(friendRequest.getSender().getEmail(),
-//                friendRequest.getReceiver().getName() + "님이 친구 요청을 거절했습니다.");
-//
-//        return FriendRequestResponse.from(friendRequest);
-//    }
+        friendRequest.accept();
+        friendRequestRepository.save(friendRequest);
+        friendShipService.createFriendShip(friendRequest.getSender(), friendRequest.getReceiver());
+
+        kafkaProduceService.sendNotification(friendRequest.getSender().getEmail(),
+                friendRequest.getReceiver().getName() + "님이 친구 요청을 수락했습니다.");
+
+        return FriendRequestResponse.from(friendRequest);
+    }
+
+    @Transactional
+    public FriendRequestResponse rejectFriendRequest(Long friendRequestId) {
+        FriendRequest friendRequest = friendRequestRepository.findById(friendRequestId)
+                .orElseThrow(() -> FRIEND_REQUEST_NOT_FOUND);
+        validateRequestReceiverIsCurrentUser(friendRequest);
+        validateFriendRequestStatus(friendRequest);
+
+        friendRequest.reject();
+        friendRequestRepository.save(friendRequest);
+
+        kafkaProduceService.sendNotification(friendRequest.getSender().getEmail(),
+                friendRequest.getReceiver().getName() + "님이 친구 요청을 거절했습니다.");
+
+        return FriendRequestResponse.from(friendRequest);
+    }
 
     private void validateFriendRequestStatus(FriendRequest friendRequest) {
         if(friendRequest.getFriendRequestStatus() != PENDING){
