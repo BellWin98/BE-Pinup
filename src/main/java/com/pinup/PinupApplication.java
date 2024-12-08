ec6073b15c4ab52f30780ac82b62f5af33b36aed
Bellwin98 <jongseunghan@naver.com>
2024-12-08 22:50:54 +0900

    [Add] 타임존(KST) 설정 추가

--- src/main/java/com/pinup/PinupApplication.java
+++ src/main/java/com/pinup/PinupApplication.java
@@ -1,8 +1,11 @@
 package com.pinup;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing // JPA Auditing 어노테이션 전체 활성화하는 기능
@@ -10,6 +13,11 @@ public class PinupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PinupApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
