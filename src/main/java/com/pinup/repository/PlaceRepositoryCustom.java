4b636ae1f7413147f53a125b9697410d79016ff1
Bellwin98 <jongseunghan@naver.com>
2024-12-08 22:04:54 +0900

    [Remove] 불필요 파일 삭제

--- src/main/java/com/pinup/repository/PlaceRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.pinup.repository;
-
-import com.pinup.dto.response.PlaceReviewInfoDto;
-import com.pinup.entity.Member;
-
-public interface PlaceRepositoryCustom {
-
-    PlaceReviewInfoDto findPlaceReviewInfo(String kakaoMapId, Member currentMember);
-}
